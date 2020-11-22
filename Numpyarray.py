1d9792861f2d4f036943228af0d8e0d1037098e6
HyperGDX <1050264043@qq.com>
2020-11-22 17:16:35 +0800

    11.22

--- Numpyarray.py
+++ Numpyarray.py
@@ -69,6 +69,10 @@ a = np.array([[1, 2, 3], [3, 4, 5], [5, 6, 7]])
 print(a)
 print(a[1])
 print(a[1:])
-print(a[...,1])
-print(a[1,...])
-print(a[...,1:])
+print(a[..., 1])
+print(a[1, ...])
+print(a[..., 1:])
+# 整数数组切片
+a = np.array([[1, 2], [3, 4], [5, 6]])
+b = a[[0, 1, 2], [0, 1, 0]]
+print(b)
