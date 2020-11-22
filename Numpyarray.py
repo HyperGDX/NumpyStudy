aea86d87af7ff945fd2f10436a350101cd55fbe5
HyperGDX <1050264043@qq.com>
2020-11-22 17:51:46 +0800

    11.22

--- Numpyarray.py
+++ Numpyarray.py
@@ -76,3 +76,15 @@ print(a[..., 1:])
 a = np.array([[1, 2], [3, 4], [5, 6]])
 b = a[[0, 1, 2], [0, 1, 0]]
 print(b)
+
+x = np.array([[  0,  1,  2],[  3,  4,  5],[  6,  7,  8],[  9,  10,  11]])  
+print ('我们的数组是：' )
+print (x)
+print ('\n')
+rows = np.array([[0,0],[3,3]]) 
+print(rows)
+cols = np.array([[0,2],[0,2]]) 
+print(cols)
+y = x[rows,cols]  
+print  ('这个数组的四个角元素是：')
+print (y)
