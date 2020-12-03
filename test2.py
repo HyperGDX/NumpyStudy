3b5d8453e5d94680f2b642c64a4b1eb18023a774
HyperGDX <1050264043@qq.com>
2020-12-03 19:39:52 +0800

    12.3

--- test2.py
+++ test2.py
@@ -1,6 +1,23 @@
-while True:
-    print("a")
-    if(input()=="q"):
-        break
+from matplotlib.pyplot import polar, xticks
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-10, 10, 0.1)
+y = x**2
+plt.xlim(-10, 10)
+plt.ylim(0, 100)
+#plt.plot(x, y)
+# plt.show()
 
-print("b")
+x = np.linspace(-np.pi, np.pi)
+y1 = np.cos(x)
+xticks(np.linspace(-np.pi, np.pi, 5))
+#plt.plot(x, y1)
+# plt.show()
+
+alpha=1
+theta=np.linspace(0,2*np.pi, 500)
+r=np.sqrt(2*(alpha**2)*np.cos(2*theta))
+plt.axes(polar=True)
+plt.plot(theta,r)
+plt.show()
+
